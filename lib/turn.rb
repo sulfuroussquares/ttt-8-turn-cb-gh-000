433363fbe001529d956168f5b2f7eaa8cd1bb739
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-10 20:31:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,1 @@
-def input_to_index(input)
-  input -= 1
-  return input
 
-def display_board(board)
-    puts " #{board[0]} " + "|"+ " #{board[1]} " + "|"+  + " #{board[2]} "
-    puts "-----------"
-    puts " #{board[3]} " + "|"+ " #{board[4]} " + "|"+  + " #{board[5]} "
-    puts "-----------"
-    puts " #{board[6]} " + "|"+ " #{board[7]} " + "|"+  + " #{board[8]} "
-end
