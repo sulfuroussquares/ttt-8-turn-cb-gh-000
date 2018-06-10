4a6aa4a40d90f1e415d4899b7507b1b64aa3555d
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-10 20:54:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,9 +6,7 @@ def display_board(board)
     puts " #{board[6]} " + "|"+ " #{board[7]} " + "|"+  + " #{board[8]} "
 end
 
-def input_to_index(number)
-  return new_number -= 1
-end
+
 
 def turn(board)
   puts "Please enter 1-9:"
